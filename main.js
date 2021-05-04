26f925b686a69701b9351dc2a730a1b4c4d34696
Hoàng Kiệt <75035735+hoangkiet1906@users.noreply.github.com>
2021-05-04 21:29:46 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Vlava - v2.2.1
-* Template URL: https://bootstrapmade.com/vlava-free-bootstrap-one-page-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 !(function($) {
   "use strict";
 
